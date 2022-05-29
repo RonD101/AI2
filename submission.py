6bc2d2250ca1d8420eed24492ffbba596180523e
renenkantor <renenkantor@campus.technion.ac.il>
2022-05-29 12:29:10 +0300

    minimax wins but its weird

--- submission.py
+++ submission.py
@@ -60,28 +60,42 @@ def cash_diff(env: TaxiEnv, taxi_id):
 
 class AgentMinimax(Agent):
     def run_step(self, env: TaxiEnv, agent_id, time_limit):
-        opponent = (agent_id + 1) % 2
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        for child, op in zip(children, operators):
+            child.apply_operator(agent_id, op)
         start_time = time.time()
-        spare_for_checking = time_limit * 0.5
-        legal_ops = env.get_legal_operators(agent_id)
-        cur_op = legal_ops[0]
-        cur_depth = 0
-        while (time.time() - start_time < spare_for_checking) or (cur_depth > env.num_steps):
-            legal_ops = env.get_legal_operators(agent_id)
-            children = [env.clone() for _ in legal_ops]
-            for child, op in zip(children, legal_ops):
-                child.apply_operator(agent_id, op)
-            possible_moves_for_opp = [self.RB_algo(child, opponent, cur_depth) for child in children]
-            best_move_for_opp = max(possible_moves_for_opp)
-            cur_op = legal_ops[best_move_for_opp.index()]
-            cur_depth += 1
-        return cur_op
+        time_to_run_algo = time_limit * 0.1
+        depth = 0
+        index_selected = 0
+        while (time.time() - start_time < time_to_run_algo):
+            children_heuristics = [RB_minimax(child, agent_id, depth, True) for child in children]
+            depth += 1          
+            max_heuristic = max(children_heuristics)
+            index_selected = children_heuristics.index(max_heuristic) 
+        print(operators[index_selected])
+        return operators[index_selected]
 
-    def RB_algo(self, env: TaxiEnv, agent_id, depth: int):
-        if env.done():
-            return env.get_balances()
-        if depth == 0:
-            return imp_heuristic(env, agent_id)
+def RB_minimax(env: TaxiEnv, agent_id: int, depth: int, is_agent_turn: bool):
+    if env.done():
+        return env.get_balances()
+    if depth == 0:
+        return imp_heuristic(env, agent_id)
+    legal_ops = env.get_legal_operators(agent_id)
+    children = [env.clone() for _ in legal_ops]
+    if is_agent_turn:
+        cur_max = -sys.maxsize - 1
+        for child in children:
+            v_max = RB_minimax(child, agent_id, depth - 1, False)
+            cur_max = max(cur_max, v_max)
+        return cur_max
+    else: 
+        cur_min = sys.maxsize
+        for child in children:
+            v_max = RB_minimax(child, agent_id, depth - 1, True)
+            cur_min = min(cur_min, v_max)
+        return cur_min
+
 
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
